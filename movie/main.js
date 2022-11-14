6b2bfc292cb27468cfd162c71dca4eafbf61ccf8
Mostafa Esmail <mostafaghareeb132@gmail.com>
2022-11-14 06:27:41 +0200

    modify styling

--- movie/main.js
+++ movie/main.js
@@ -95,7 +95,7 @@ const loadVideo = async () => {
       );
     })
     .map((videoTrailer) => {
-      return `<iframe width="80%" height="500" src="https://www.youtube.com/embed/${videoTrailer.key}" title="YouTube video player"
+      return `<iframe width="100%" height="400" src="https://www.youtube.com/embed/${videoTrailer.key}" title="YouTube video player"
        frameborder="3" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;
         picture-in-picture" allowfullscreen></iframe>`;
     })
